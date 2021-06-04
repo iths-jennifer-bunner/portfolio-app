28a063342d1035096b2ab9a7e7e6d894159a0a6c
iths-jennifer-bunner <jenniferbunner@hotmail.com>
2021-06-05 00:11:08 +0200

    removed email

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,18 +6,19 @@ function Footer() {
     <div className="footer">
       <div className="footer__leftDiv">
         <h2>KONTAKT</h2>
-        <p>bunner.jennifer@gmail.com</p>
-      </div>
-      <div className="footer__rightDiv">
-        <div className="footer__icon">
-          <a href="https://www.linkedin.com/in/jenniferbunner810">
-            <i className="fab fa-linkedin"></i>
-          </a>
-        </div>
-        <div className="footer__icon">
-          <a href="https://github.com/iths-jennifer-bunner">
-            <i className="fab fa-github-square"></i>
-          </a>
+        {/* <p>bunner.jennifer@gmail.com</p> */}
+
+        <div className="footer__rightDiv">
+          <div className="footer__icon">
+            <a href="https://www.linkedin.com/in/jenniferbunner810">
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </div>
+          <div className="footer__icon">
+            <a href="https://github.com/iths-jennifer-bunner">
+              <i className="fab fa-github-square"></i>
+            </a>
+          </div>
         </div>
       </div>
     </div>
