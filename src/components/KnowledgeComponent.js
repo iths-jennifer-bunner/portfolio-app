b769f67e8791c5987429ab015ef8b081b6435e19
iths-jennifer-bunner <jenniferbunner@hotmail.com>
2021-06-07 14:47:03 +0200

    added text content

--- src/components/KnowledgeComponent.js
+++ src/components/KnowledgeComponent.js
@@ -7,24 +7,40 @@ function KnowledgeComponent({ reference }) {
       <h3>KUNSKAPER</h3>
       <div className="knowledge__container">
         <p>
-          Under snart 2 år har jag fått lära mig allt från Html, Css och
-          Javascript till att jobba med projekt med agila metoder, scrum och
-          git. Vi har haft stort fokus på React.js men även node.js, apier,
-          bygga sideor med egna teman och plugins i Wordpress och Php samt att
-          styla med Sass/Scss och BEM-metodiken. Utbildningen har varit väldigt
-          bred men uppskattar att jag har fått prova på så mycket och jag gillar
-          att lära mig nya saker och jag lär mig snabbt. Jag brinner lite extra
-          för design, färg och form men vill gärna lära fördjupa mina kunskaper
-          i tex. React även om det är där jag har mest grund ifrån min
-          utbildning och praktik så kan man alltid utvecklas mer.
+          Under de två år jag har gått på IT-högskolan har jag fått bra
+          kunskaper i HTML, CSS och Javascript. Jag har också goda kunskaper i
+          Sass och Bem-metodiken. Jag har gjort flera projekt i React.js och på
+          min första Lia så kodade jag en sida i Vue.js.
+        </p>
+        <p>
+          Vi har haft en omfattande kurs i Grafisk- och UX design, där vi har
+          lärt oss flera begrepp, metoder, processer och verktyg som används
+          inom User Experience. Vi har jobbat med Agil utveckling såsom Scrum
+          och Kanban, sprint-planering, User Stories bla och det har jag sedan
+          fått mer erfarenhet efter mina Lior som har jobbat med SCRUM.
+          Kombinerat med kursen Agila metoder och Kommunikation, Kravhantering
+          och Projektstyrning så jobbade vi med CMS och i detta fall Wordpress.
+          Där har vi fått skapa egna webbsidor, e-handel och i grupparbete
+          skapat eget tema till en fiktiv hemsida, med custom fields och Session
+          variables. Så jag har grundläggande kunskaper i Php. Vi har även fått
+          öva på backend-programmering med Node.js och pakethantering med NPM
+          och prestandaoptimering.
+        </p>
+        <p>
+          Utbildningen har varit väldigt bred men jag uppskattar att jag har
+          fått prova på så mycket och jag gillar att lära mig nya saker och jag
+          lär mig snabbt. Jag brinner lite extra för design, färg och form men
+          vill gärna lära fördjupa mina kunskaper i tex. React.js även om det är
+          där jag har mest grund ifrån min utbildning och praktik så kan man
+          alltid utvecklas mer.
         </p>
         <p>
           Under mina Lior har jag fått en bra bild över hur det fungerar att
           arbeta i team både remote men även inhouse. På min första lia fick jag
           i uppdrag att skapa från design till development deras produktsida:
-          www.bocapital.se. som jag kodat helt själv med Vue.js och Quasar
-          Framework. Jag översatte även sidan med hjälp av i18n samt
-          installerade MailChimp.
+          <a href="www.bocapital.se"> www.bocapital.se</a>. som jag kodat helt
+          själv med Vue.js och Quasar Framework. Jag översatte även sidan med
+          hjälp av i18n samt installerade MailChimp.
         </p>
         <p>
           Under min andra lia hos ZignSec fick jag och en till praktikant i
